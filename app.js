00b5f71b238459b78f97a047fb8cc0c9a93bb427
gyanendra9984 <mani15102002@gmail.com>
2023-10-25 13:02:33 +0530

    calculator completed

--- app.js
+++ app.js
@@ -4,28 +4,47 @@ const map = { 'E': '*10**', '÷': '/', '×': '*', 'sin(': 'Math.sin(', 'cos(': '
 const inverse = { 'sin': 'sin<sup>-1</sup>', 'cos': 'cos<sup>-1</sup>', 'tan': 'tan<sup>-1</sup>', 'ln': 'e<sup>x</sup>', 'log': '10<sup>x</sup>', '√': 'x<sup>2</sup>', 'x<sup>y</sup>': '<sup>y</sup>√x', 'Ans': 'Rnd' }
 const changeVal = { 'x!': '!', '<sup>n</sup>C<sub>r</sub>': 'C', '<sup>n</sup>P<sub>r</sub>': 'P', 'x<sup>y</sup>': '^', 'ln': 'ln(', 'log': 'log(', 'sin': 'sin(', 'cos': 'cos(', 'tan': 'tan(', 'sin<sup>-1</sup>': 'sin<sup>-1</sup>(', 'cos<sup>-1</sup>': 'cos<sup>-1</sup>(', 'tan<sup>-1</sup>': 'tan<sup>-1</sup>(', 'e<sup>x</sup>': 'e^', 'x<sup>y</sup>': '^', 'x<sup>2</sup>': '^2', '10<sup>x</sup>': '10^', '√': '√(', 'EXP': 'E', '<sup>y</sup>√x': '√' }
 
-function fat(n){
+function fat(n) {
     if (n === 0) {
-        return 1; 
+        return 1;
     } else if (n < 0) {
         return "Invalid input";
     } else {
         return n * fat(n - 1);
     }
 }
+var operation = ['+', '-', '÷', '×', 'P', 'C', '^', '%', '!', '√', 'E','^2'];
+var func = ['ln(', 'log(', 'sin(', 'cos(', 'tan(', 'sin<sup>-1</sup>(', 'cos<sup>-1</sup>', 'tan<sup>-1</sup>(', 'e^', '10^', '√('];
+
 let buttons = document.getElementsByTagName('button')
 for (let i = 0; i < buttons.length; i++) {
     if (!notToDisplay.includes(buttons[i].className)) {
         buttons[i].addEventListener('click', () => {
-            let display = document.getElementsByClassName('exp')[0], x = buttons[i].innerHTML
-            if (Object.keys(changeVal).includes(x)) {
-                display.innerHTML += changeVal[x]
-            }
-            else if (x == 'Rnd') {
+            let display = document.getElementsByClassName('exp')[0];
+            let x = buttons[i].innerHTML
+            if (x == 'Rnd') {
                 display.innerHTML += Math.random().toFixed(5)
-            }
-            else {
-                display.innerHTML += x
+            } else if (Object.keys(changeVal).includes(x)) {
+                x = changeVal[x];
+                if (operation.includes(x)) {
+                    if (/[πe)]|[0-9]/.test(display.innerHTML.slice(-1))) {
+                        display.innerHTML += x;
+                    }
+                } else if (func.includes(x)) {
+                    if (!(/[πe)]|[0-9]/.test(display.innerHTML.slice(-1)))) {
+                        display.innerHTML += x;
+                    }
+                } else {
+                    display.innerHTML += x;
+                }
+            } else {
+                if (operation.includes(x)) {
+                    if (/[πe)]|[0-9]/.test(display.innerHTML.slice(-1))) {
+                        display.innerHTML += x;
+                    }
+                } else {
+                    display.innerHTML += x;
+                }
             }
         })
     }
@@ -36,24 +55,24 @@ let eql = document.getElementsByClassName('eql')[0]
 eql.addEventListener('click', () => {
     let x = exp.innerHTML.trim();
 
-     var re = /([0-9]*)!/ig;
-     var match;
+    var re = /([0-9]+)!/ig;
+    var match;
     while ((match = re.exec(x)) != null) {
-        x = x.replace(/([0-9]*)!/ig, `fat(${match[1]})`);
+        x = x.replace(/([0-9]+)!/ig, `fat(${match[1]})`);
     }
 
-     re = /([0-9]*)P([0-9]*)/ig;
+    re = /([0-9]+)P([0-9]+)/ig;
     while ((match = re.exec(x)) != null) {
-        x = x.replace(/([0-9]*)P([0-9]*)/ig, `fat(${match[1]})/fat(${match[1]}-${match[2]})`);
+        x = x.replace(/([0-9]+)P([0-9]+)/ig, `fat(${match[1]})/fat(${match[1]}-${match[2]})`);
     }
 
 
-       re = /([0-9]*)C([0-9]*)/ig;
+    re = /([0-9]+)C([0-9]+)/ig;
     while ((match = re.exec(x)) != null) {
-        x = x.replace(/([0-9]*)C([0-9]*)/ig, `fat(${match[1]})/(fat(${match[1]}-${match[2]})*fat(${match[2]}))`);
+        x = x.replace(/([0-9]+)C([0-9]+)/ig, `fat(${match[1]})/(fat(${match[1]}-${match[2]})*fat(${match[2]}))`);
     }
 
-    re = /([0-9]*)√([0-9]*)/ig;
+    re = /([0-9]+)√([0-9]+)/ig;
     while ((match = re.exec(x)) != null) {
         x = x.replace(/([0-9]+)√([0-9]+)/ig, `Math.pow(${match[2]},1/${match[1]})`);
     }
--- text.js
+++ text.js
@@ -1,7 +1,7 @@
 // console.log(stringWithLastSubRemoved);
 
-// const s = '3 + 2 + 4 + 5'
-// const regex = /([0-9]*)\s\+\s([0-9]*)v/;
+// const s = '(())'
+// const regex = /(${regex})*/;
 // const matches = s.match(regex)
 // console.log(matches)
 
@@ -10,15 +10,15 @@
 // const matches = regex.exec(s)
 // console.log(matches)
 // console.log(s);
-var str = "2!+3!";
+// var str = "2!+3!";
 
-var re = /([0-9]*)!/ig;
-var match;
-while ((match = re.exec(str)) != null){
-    console.log(match);
-}
-console.log(str str);
-// console.log();
+// var re = /([0-9]*)!/ig;
+// var match;
+// while ((match = re.exec(str)) != null){
+//     console.log(match);
+// }
+// console.log(str str);
+// // console.log();
 
 // const inputString = "This is a √ symbol.";
 // const regex = /√/;
@@ -28,3 +28,29 @@ console.log(str str);
 // } else {
 //   console.log("No match found.");
 // }
+
+// const values = ['apple', 'banana', 'cherry', 'date'];
+
+// // Construct a regular expression pattern that matches any single value from the array
+// const pattern = values.join('|');
+
+// const input = 'I have a banana and a date.';
+// const matches = input.match(pattern);
+// console.log(pattern);
+
+// if (matches) {
+//   console.log(matches);
+// } else {
+//   console.log('No matches found.');
+// }
+// console.log(eval(Math.pow(Math.sqrt(9),Math.pow(2,2))));
+
+
+const pattern = /[πe)]/;
+const input = 'gyanendrπ';
+
+if (/[πe)]/.test(input.slice(-1))) {
+  console.log("The pattern matches the input string.");
+} else {
+  console.log("The pattern does not match the input string.");
+}
